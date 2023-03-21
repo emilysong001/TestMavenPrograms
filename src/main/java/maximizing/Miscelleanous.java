2bad13658619b44a26dcccc095a70569d17b39dd
emilysong001 <emily001.song@gmail.com>
2023-03-21 15:35:48 -0400

    get screenshot practice

--- src/main/java/maximizing/Miscelleanous.java
+++ src/main/java/maximizing/Miscelleanous.java
@@ -1,17 +1,28 @@
 package maximizing;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.io.File;
+import java.io.IOException;
 
 public class Miscelleanous {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        ChromeOptions option = new ChromeOptions();
+        option.addArguments("--remote-allow-origins=*");
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new ChromeDriver(option);
 
         driver.manage().window().maximize();
         //driver.manage().deleteAllCookies();
-        driver.manage().deleteCookieNamed("sfd");
+        //driver.manage().deleteCookieNamed("sfd");
         driver.get("https://google.com");
+        File src =((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(src, new File("C:\\Users\\xuanx\\IdeaProjects\\TestMavenPrograms\\screenshot.png"));
     }
 }
